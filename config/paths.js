d136581c146b8fe8ae4e1b664e5be6af9d51db27
zhoudonghui <zhoudonghui@qutoutiao.net>
2020-05-05 20:23:25 +0800

    merge master

--- config/paths.js
+++ config/paths.js
@@ -16,10 +16,9 @@ const resolveApp = relativePath => path.resolve(appDirectory, relativePath);
 // We can't use a relative path in HTML because we don't want to load something
 // like /todos/42/static/js/bundle.7289d.js. We have to know the root.
 const publicUrlOrPath = getPublicUrlOrPath(
-<<<<<<< HEAD
-    // process.env.NODE_ENV === 'development',
+    process.env.NODE_ENV === 'development',
     require(resolveApp('package.json')).homepage,
-    // process.env.PUBLIC_URL
+    process.env.PUBLIC_URL
 );
 
 const moduleFileExtensions = [
@@ -34,30 +33,10 @@ const moduleFileExtensions = [
     'json',
     'web.jsx',
     'jsx',
-=======
-  process.env.NODE_ENV === 'development',
-  require(resolveApp('package.json')).homepage,
-  process.env.PUBLIC_URL
-);
-
-const moduleFileExtensions = [
-  'web.mjs',
-  'mjs',
-  'web.js',
-  'js',
-  'web.ts',
-  'ts',
-  'web.tsx',
-  'tsx',
-  'json',
-  'web.jsx',
-  'jsx',
->>>>>>> master
 ];
 
 // Resolve file paths in the same order as webpack
 const resolveModule = (resolveFn, filePath) => {
-<<<<<<< HEAD
     const extension = moduleFileExtensions.find(extension =>
         fs.existsSync(resolveFn(`${filePath}.${extension}`))
     );
@@ -67,22 +46,10 @@ const resolveModule = (resolveFn, filePath) => {
     }
 
     return resolveFn(`${filePath}.js`);
-=======
-  const extension = moduleFileExtensions.find(extension =>
-    fs.existsSync(resolveFn(`${filePath}.${extension}`))
-  );
-
-  if (extension) {
-    return resolveFn(`${filePath}.${extension}`);
-  }
-
-  return resolveFn(`${filePath}.js`);
->>>>>>> master
 };
 
 // config after eject: we're in ./config/
 module.exports = {
-<<<<<<< HEAD
     dotenv: resolveApp('.env'),
     appPath: resolveApp('.'),
     appBuild: resolveApp('build'),
@@ -98,23 +65,6 @@ module.exports = {
     proxySetup: resolveApp('src/setupProxy.js'),
     appNodeModules: resolveApp('node_modules'),
     publicUrlOrPath,
-=======
-  dotenv: resolveApp('.env'),
-  appPath: resolveApp('.'),
-  appBuild: resolveApp('build'),
-  appPublic: resolveApp('public'),
-  appHtml: resolveApp('public/index.html'),
-  appIndexJs: resolveModule(resolveApp, 'src/index'),
-  appPackageJson: resolveApp('package.json'),
-  appSrc: resolveApp('src'),
-  appTsConfig: resolveApp('tsconfig.json'),
-  appJsConfig: resolveApp('jsconfig.json'),
-  yarnLockFile: resolveApp('yarn.lock'),
-  testsSetup: resolveModule(resolveApp, 'src/setupTests'),
-  proxySetup: resolveApp('src/setupProxy.js'),
-  appNodeModules: resolveApp('node_modules'),
-  publicUrlOrPath,
->>>>>>> master
 };
 
 
