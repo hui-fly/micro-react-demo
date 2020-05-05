b466f615c39bac46e69163f5a745ac25f5ac3a1d
zhoudonghui <zhoudonghui@qutoutiao.net>
2020-05-05 21:10:38 +0800

    deploy

---
+++ precache-manifest.78252c8f7ed2f8fd6ecea30156ca236f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c58744adfbdbcbe67d7",
+    "url": "/micro-react-demo/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "934c79c8b2b1a92434197435a5cb21ee",
+    "url": "/micro-react-demo/index.html"
+  },
+  {
+    "revision": "a9d2aba1b25b939ca513",
+    "url": "/micro-react-demo/js/2.9f4f5b31.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/micro-react-demo/js/2.9f4f5b31.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c83139725ae300da449f",
+    "url": "/micro-react-demo/js/app.js"
+  },
+  {
+    "revision": "3c58744adfbdbcbe67d7",
+    "url": "/micro-react-demo/js/main.1f2d991c.chunk.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/micro-react-demo/media/logo.5d5d9eef.svg"
+  }
+]);
